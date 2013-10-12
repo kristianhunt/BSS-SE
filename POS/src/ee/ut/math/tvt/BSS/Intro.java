68707649665497106261d436c109a31e7b77ded1
SilverT <silvertiik7@gmail.com>
2013-10-12 20:23:12 +0300

    UI

--- POS/src/ee/ut/math/tvt/BSS/Intro.java
+++ POS/src/ee/ut/math/tvt/BSS/Intro.java
@@ -1,5 +1,14 @@
 package ee.ut.math.tvt.BSS;
 
-public class Intro {
+import javax.swing.JFrame;
 
+public class Intro {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		IntroUI a = new IntroUI();
+		a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
