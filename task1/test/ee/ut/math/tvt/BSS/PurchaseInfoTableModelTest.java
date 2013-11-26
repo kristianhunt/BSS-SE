915f54f8eeb98cb4c79b6ca3e0fbf2ebd9eb0d58
DenisZhadan <denis@ut.ee>
2013-11-26 21:10:32 +0200

    PurchaseInfoTableModelTest fix

--- task1/test/ee/ut/math/tvt/BSS/PurchaseInfoTableModelTest.java
+++ task1/test/ee/ut/math/tvt/BSS/PurchaseInfoTableModelTest.java
@@ -33,21 +33,21 @@ public class PurchaseInfoTableModelTest {
 		double return_price = ((Number) (Object) model.getColumnValue(item, 2)).doubleValue();
 		assertEquals(return_price, item_price, 0.001);
 
-		Integer return_quantity = ((Integer) model.getColumnValue(item, 3)).intValue();
+		Integer return_quantity = ((Integer) model.getColumnValue(item, 3)).intValue();		
 		assertEquals(return_quantity.intValue(), sold_quantity);
 
-		double return_sum =  ((Number) (Object) model.getColumnValue(item, 4)).doubleValue();
+		double return_sum = ((Number) (Object) model.getColumnValue(item, 4)).doubleValue();
 		assertEquals(return_sum, sold_quantity * item_price, 0.0001);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void testHasEnoughInStock() {
+	public void testGetColumnByIllegalId() {
 		model.addItem(item);
 		model.getColumnValue(item, 5);
 	}
 
 	@Test(expected = java.util.NoSuchElementException.class)
-	public void testQuantityInBasketByBarCodeException() {
+	public void testInBasketNoSuchElementException() {
 		model.getItemById((long) 20);
 	}
 
