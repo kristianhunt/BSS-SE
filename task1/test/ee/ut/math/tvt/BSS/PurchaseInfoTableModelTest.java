27cb0bf0737e3b0284f93bace7632fcb4f9ee7c2
DenisZhadan <denis@ut.ee>
2013-11-26 20:24:48 +0200

    optimization for clear vision

--- task1/test/ee/ut/math/tvt/BSS/PurchaseInfoTableModelTest.java
+++ task1/test/ee/ut/math/tvt/BSS/PurchaseInfoTableModelTest.java
@@ -11,27 +11,39 @@ import ee.ut.math.tvt.salessystem.ui.model.PurchaseInfoTableModel;
 public class PurchaseInfoTableModelTest {
 
 	PurchaseInfoTableModel model = new PurchaseInfoTableModel();
-	SoldItem i1 = new SoldItem(new StockItem((long) 1, "testItem1", "desc1", 3,
-			35), 2);
+
+	Long item_id = new Long(1);
+	String item_name = "testItem1";
+	String item_desc = "desc1";
+	double item_price = 3;
+	int item_quantity = 35;
+	int sold_quantity = 2;
+
+	SoldItem item = new SoldItem(new StockItem(item_id, item_name, item_desc,
+			item_price, item_quantity), sold_quantity);
 
 	@Test
 	public void testGetColumnValue() {
-		long id = ((Long) model.getColumnValue(i1, 0)).longValue();
-		assertEquals(id, 1, 0.0001);
-		String name = (String) model.getColumnValue(i1, 1);
-		assertEquals(name, "testItem1");
-		double price = ((Double) model.getColumnValue(i1, 2)).doubleValue();
-		assertEquals(price, 3.35, 0.001);
-		int quantity = ((Integer) model.getColumnValue(i1, 3)).intValue();
-		assertEquals(quantity, 2);
-		double sum = ((Double) model.getColumnValue(i1, 4)).doubleValue();
-		assertEquals(sum, 6.70, 0.0001);
+		Long return_id = (Long) model.getColumnValue(item, 0);
+		assertEquals(return_id.longValue(), item_id.longValue(), 0.0001);
+
+		String return_name = (String) model.getColumnValue(item, 1);
+		assertEquals(return_name, item_name);
+
+		double return_price = ((Number) (Object) model.getColumnValue(item, 2))	.doubleValue();
+		assertEquals(return_price, item_price, 0.001);
+
+		Integer return_quantity = ((Integer) model.getColumnValue(item, 3)).intValue();
+		assertEquals(return_quantity.intValue(), item_quantity);
+
+		double return_sum =  ((Number) (Object) model.getColumnValue(item, 4))	.doubleValue();
+		assertEquals(return_sum, sold_quantity * item_price, 0.0001);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testHasEnoughInStock() {
-		model.addItem(i1);
-		model.getColumnValue(i1, 5);
+		model.addItem(item);
+		model.getColumnValue(item, 5);
 	}
 
 	@Test(expected = java.util.NoSuchElementException.class)
